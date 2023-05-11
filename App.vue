48976bc4f9fb4a58b1eacd244326eeb64093cbd9
牛洪法 <1242849166@qq.com>
2023-05-11 10:09:51 +0800

    init

---
+++ App.vue
@@ -0,0 +1,11 @@
+<script>
+	export default {
+		onLaunch() {
+			console.log('App Launch')
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	/*每个页面公共css */
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+	<view class="content" @click="addNum">
+		{{num}}
+	</view>
+</template>
+
+<script setup scoped>
+	import { ref } from "vue";
+	
+	// About num
+	const num = ref(0);
+	const addNum = () => num.value ++;
+	
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
